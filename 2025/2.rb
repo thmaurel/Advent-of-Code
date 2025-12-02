88dbfc8a909dc0ec5c2827e8fff5fa033594d23b
Thibaud Maurel <maurel.thibaud@free.fr>
2025-12-02 06:26:04 +0100

    2025 day 2

--- 2025/2.rb
+++ 2025/2.rb
@@ -6,19 +6,16 @@ res2 = 0
 d.each do |l|
   a = l.split('-').first.to_i
   b = l.split('-').last.to_i
+
   (a..b).to_a.each do |n|
-    k = n.to_s.length
+    ns = n.to_s
+    k = ns.length
     next if k == 1
 
-    res1 += n if n.to_s[0..k/2 - 1] == n.to_s[k/2..k-1]
+    res1 += n if ns[0..k / 2 - 1] == ns[k / 2..k - 1]
 
-    ns = n.to_s
-    if ns.split('').uniq.length == 1
-      res2 += n
-      next
-    end
-    (2..k/2).to_a.each do |j|
-      if k % j == 0 && ns == (ns[0..j - 1] * (k /j))
+    (1..k / 2).to_a.each do |j|
+      if k % j == 0 && ns == (ns[0..j - 1] * (k / j))
         res2 += n
         break
       end
